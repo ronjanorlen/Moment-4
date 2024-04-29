ce3af2313e35a9d24a34f3e63d1e1c923f8c845c
Ronja Norlén <rono2300@student.miun.se>
2024-04-29 12:01:46 +0200

    Lägger in webtoken och testar i thunderclient

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 /* Anslut till mongoDB */
@@ -62,7 +63,14 @@ router.post("/login", async(req, res) => {
         if(!isPasswordMatch) {
             return res.status(401).json({ error: "Felaktiga användaruppgifter "});
         } else {
-            res.status(200).json({ message: "Användare inloggad" });
+            // Skapa JWT
+            const payload = { username: username };
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+            const response = {
+                message: "Användare inloggad",
+                token: token
+            }
+            res.status(200).json({ response });
         }
 
     } catch (error) {
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,25 @@ app.use(bodyParser.json());
 /* Routes */
 app.use("/api", authRoutes);
 
+/* Skyddad route */
+app.get("/api/protected", authenticateToken, (req, res) => {
+    res.json({ message: "Skyddad route!" });
+});
+
+/* Validera token */
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Token
+
+    if(token == null) res.status(401).json({ message: "Not authorized for this route - token missing" });
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) return res.status(403).json({ message: "Invalid JWT" });
+
+        req.username = username; 
+        next();
+    });
+}
 /* Starta applikationen */
 app.listen(port, () => {
     console.log("Server running at port: " + port);
